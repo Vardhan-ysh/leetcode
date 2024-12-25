f9aef4e35cd6d2189e9f71aba059bfef0fbbd31a
Yashvardhan Singh <yashvardhansingh112233@gmail.com>
2024-12-25 22:59:33 +0530

    Time: 2 ms (10.41%) | Memory: 45.1 MB (20.71%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -1,30 +1,45 @@
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        Queue<Pair> q = new LinkedList<>();
         int r = image.length;
         int c = image[0].length;
-        int originalColor = image[sr][sc];
+        boolean[][] visited = new boolean[r][c];
+        int original = image[sr][sc];
 
-        if (originalColor == color) return image;
+        q.add(new Pair(sr, sc));
 
-        int[][] directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-        Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{sr, sc});
+        while(!q.isEmpty()) {
+            Pair p = q.poll();
+            visited[p.a][p.b] = true;
+            image[p.a][p.b] = color;
 
-        while (!q.isEmpty()) {
-            int[] current = q.poll();
-            int row = current[0];
-            int col = current[1];
+            boolean up = !(p.a == 0);
+            boolean down = !(p.a == (r - 1));
+            boolean left = !(p.b == 0);
+            boolean right = !(p.b == (c - 1));
 
-            image[row][col] = color;
-
-            for (int[] dir : directions) {
-                int newRow = row + dir[0];
-                int newCol = col + dir[1];
-                if (newRow >= 0 && newRow < r && newCol >= 0 && newCol < c && image[newRow][newCol] == originalColor) {
-                    q.add(new int[]{newRow, newCol});
-                }
+            if(up && !visited[p.a - 1][p.b] && image[p.a - 1][p.b] == original) {
+                q.offer(new Pair(p.a - 1, p.b));
+            }
+            if(down && !visited[p.a + 1][p.b] && image[p.a + 1][p.b] == original) {
+                q.offer(new Pair(p.a + 1, p.b));
+            }
+            if(left && !visited[p.a][p.b - 1] && image[p.a][p.b - 1] == original) {
+                q.offer(new Pair(p.a, p.b - 1));
+            }
+            if(right && !visited[p.a][p.b + 1] && image[p.a][p.b + 1] == original) {
+                q.offer(new Pair(p.a, p.b + 1));
             }
         }
         return image;
+    }
+}
+
+class Pair {
+    int a; 
+    int b;
+    Pair(int a, int b) {
+        this.a = a;
+        this.b = b;
     }
 }
