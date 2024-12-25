cf1de75d8fb628348cee8a2e495443c3090eeda9
Yashvardhan Singh <yashvardhansingh112233@gmail.com>
2024-12-25 11:58:10 +0530

    Time: 12 ms (8.70%) | Memory: 52.7 MB (7.65%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -2,40 +2,59 @@ class Solution {
     public int numIslands(char[][] grid) {
         int l = grid.length;
         int b = grid[0].length;
-        
-        int[] dx = {-1, 1, 0, 0};
-        int[] dy = {0, 0, -1, 1};
-        
+
         int[][] visited = new int[l][b];
         Queue<Pair> q = new LinkedList<>();
         int ans = 0;
-        
+
         for(int i = 0; i < l; i++) {
             for(int j = 0; j < b; j++) {
                 if(visited[i][j] == 1 || grid[i][j] == '0') {
                     continue;
                 }
-                
-                q.add(new Pair(i, j));
+
+                Pair curr = new Pair(i, j);
+                q.add(curr);
                 visited[i][j] = 1;
                 ans++;
-                
+
                 while(!q.isEmpty()) {
                     Pair p = q.poll();
-                    
-                    for(int dir = 0; dir < 4; dir++) {
-                        int newX = p.p1 + dx[dir];
-                        int newY = p.p2 + dy[dir];
-                        
-                        if(newX >= 0 && newX < l && newY >= 0 && newY < b 
-                           && visited[newX][newY] == 0 && grid[newX][newY] == '1') {
-                            visited[newX][newY] = 1;
-                            q.offer(new Pair(newX, newY));
-                        }
+
+                    boolean up = true;
+                    boolean down = true;
+                    boolean left = true;
+                    boolean right = true;
+
+                    if(p.p1 == 0) up = false;
+                    if(p.p1 == l-1) down = false;
+                    if(p.p2 == 0) left = false;
+                    if(p.p2 == b-1) right = false;
+
+                    if(up && visited[p.p1 - 1][p.p2] == 0 && grid[p.p1 - 1][p.p2] == '1') {
+                        visited[p.p1 - 1][p.p2] = 1;
+                        Pair c = new Pair(p.p1 - 1, p.p2);
+                        q.offer(c);
+                    }
+                    if(down && visited[p.p1 + 1][p.p2] == 0 && grid[p.p1 + 1][p.p2] == '1') {
+                        visited[p.p1 + 1][p.p2] = 1;
+                        Pair c = new Pair(p.p1 + 1, p.p2);
+                        q.offer(c);
+                    }
+                    if(left && visited[p.p1][p.p2 - 1] == 0 && grid[p.p1][p.p2 - 1] == '1') {
+                        visited[p.p1][p.p2 - 1] = 1;
+                        Pair c = new Pair(p.p1, p.p2 - 1);
+                        q.offer(c);
+                    }
+                    if(right && visited[p.p1][p.p2 + 1] == 0 && grid[p.p1][p.p2 + 1] == '1') {
+                        visited[p.p1][p.p2 + 1] = 1;
+                        Pair c = new Pair(p.p1, p.p2 + 1);
+                        q.offer(c);
                     }
                 }
             }
         }
+
         return ans;
     }
 }
