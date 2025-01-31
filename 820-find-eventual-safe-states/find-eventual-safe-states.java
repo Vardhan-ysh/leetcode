73fc926dd8b05d145f1897bf6047d9ce4d3cee81
Yashvardhan Singh <yashvardhansingh112233@gmail.com>
2025-01-31 11:59:59 +0530

    Time: 4 ms (97.98%) | Memory: 55.5 MB (73.68%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.java
+++ 820-find-eventual-safe-states/find-eventual-safe-states.java
@@ -1,42 +1,34 @@
+import java.util.*;
+
 class Solution {
     public List<Integer> eventualSafeNodes(int[][] graph) {
-        Set<Integer> terminals = new HashSet<>();
-        List<Integer> ans = new ArrayList<>();
+        int n = graph.length;
+        int[] status = new int[n];
+        List<Integer> safeNodes = new ArrayList<>();
         
-        for (int i = 0; i < graph.length; i++) {
-            if (graph[i].length == 0) {
-                terminals.add(i);
-                ans.add(i);
+        for (int i = 0; i < n; i++) {
+            if (isSafe(i, graph, status)) {
+                safeNodes.add(i);
             }
         }
         
-        boolean newAdded = true;
-        while (newAdded) {
-            newAdded = false;
-            List<Integer> newSafeNodes = new ArrayList<>();
-            
-            for (int i = 0; i < graph.length; i++) {
-                if (terminals.contains(i)) continue;
-                
-                boolean check = true;
-                for (int vertex : graph[i]) {
-                    if (!terminals.contains(vertex)) {
-                        check = false;
-                        break;
-                    }
-                }
-                
-                if (check) {
-                    newSafeNodes.add(i);
-                    newAdded = true;
-                }
-            }
-            
-            ans.addAll(newSafeNodes);
-            terminals.addAll(newSafeNodes);
+        return safeNodes;
+    }
+    
+    private boolean isSafe(int node, int[][] graph, int[] status) {
+        if (status[node] != 0) {
+            return status[node] == 2;
         }
         
-        Collections.sort(ans);
-        return ans;
+        status[node] = 1;
+        for (int neighbor : graph[node]) {
+            if (status[neighbor] == 2) continue;
+            if (status[neighbor] == 1 || !isSafe(neighbor, graph, status)) {
+                return false;
+            }
+        }
+        
+        status[node] = 2;
+        return true;
     }
 }
